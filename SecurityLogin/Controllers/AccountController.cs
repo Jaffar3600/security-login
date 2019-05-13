35709aa1ac950d627ec400928abb281d74767c50
Jaffar3600 <45350559+Jaffar3600@users.noreply.github.com>
2019-05-13 09:15:55 +0000

    No changes

---
+++ SecurityLogin/Controllers/AccountController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SecurityLogin.Models;
+using System.Web.Security;
+
+namespace SecurityLogin.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult Login(Models.Membership model)
+        {
+            using ( var context = new OfficeEntities())
+            {
+                bool isValid = context.User.Any(x => x.UserName == model.Username && x.Password == model.Password);
+                if (isValid)
+                {
+                    FormsAuthentication.SetAuthCookie(model.Username, false);
+                    return RedirectToAction("Index","Employees");
+                }
+                ModelState.AddModelError("","Invalid credentials");
+                return View();
+            }
+               
+        }
+
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult SignUp(User model)
+        {
+            using (var context = new OfficeEntities())
+            {
+                context.User.Add(model);
+                context.SaveChanges();
+            }
+            return RedirectToAction("Login");
+        }
+
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login");
+        }
+    }
+}
